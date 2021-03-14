8ef36e2c06e2b2f42826f67320ace78c71f61aa3
Ian Leefmans <iel230@nyu.edu>
2021-03-14 15:17:27 -0400

    Finish making test mongodb with pymongo

--- utils/database.py
+++ utils/database.py
@@ -1,18 +1,38 @@
+import pymongo
 from pymongo import MongoClient
 
 # Create and connect to client
 client = MongoClient()
 client = MongoClient(host="localhost", port=27017) 
 
+
+
 # Create database
 db = client["RedditComments"]
+#print collections
+print(client.list_database_names())
 
-# Create Admin User for Database
-db.createUser({
-	user: "Ian" 
-	pwd: "12345"
-	roles: ["readWrite", "dbAdmin"]
-	}
-	)
+
+
+# Create collection
+collection = db["teststock"]
+# Print collections
+print(db.list_collection_names())
+
+
+
+
+
+post = {"post1": ["comment1", "comment2"], "post2": ["comment1", "comment2"]}
+
+
+
+collection.insert_one(post)
+
+
+cursor = db["teststock"].find({})
+
+for i in cursor:
+	print(i)
 
 
