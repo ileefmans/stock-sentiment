56a8ac2713f4d1ffc8426bb61e33882d0d36c873
Ian Leefmans <iel230@nyu.edu>
2021-03-14 14:07:55 -0400

    Start creating script for mongodb

--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,18 @@
-# INSERT CODE HERE
+from pymongo import MongoClient
+
+# Create and connect to client
+client = MongoClient()
+client = MongoClient(host="localhost", port=27017) 
+
+# Create database
+db = client["RedditComments"]
+
+# Create Admin User for Database
+db.createUser({
+	user: "Ian" 
+	pwd: "12345"
+	roles: ["readWrite", "dbAdmin"]
+	}
+	)
+
+
