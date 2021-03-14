144ed0600cb1bc6f860b418206f8619b467f7910
Ian Leefmans <iel230@nyu.edu>
2021-03-14 17:42:39 -0400

    file for testing scraping and inserting into DB

---
+++ utils/test.py
@@ -0,0 +1,10 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient(host="localhost", port=27017) 
+
+db = client["RedditComments"]
+
+db["stock=GME"].drop()
+
+print(db.list_collection_names())
