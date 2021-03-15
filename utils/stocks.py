72753574877e6bdd4b7f801c163dad13e7b30516
Ian Leefmans <iel230@nyu.edu>
2021-03-15 19:46:09 -0400

    Test getting current stock price interval

--- utils/stocks.py
+++ utils/stocks.py
@@ -2,6 +2,7 @@ import finnhub
 import yaml
 import pandas as pd
 import datetime
+import time
 
 
 
@@ -13,8 +14,18 @@ api_key = IDs["Finnhub"]["api_key"]
 # Set up client
 finnhub_client = finnhub.Client(api_key=api_key)
 
-res = finnhub_client.stock_candles('AAPL', 'D', 1590988249, 1591852249)
+
+# Create unix timestamp
+def create_unix_stamp(year, month, day, hour, minute, second):
+	dt = datetime.datetime(year, month, day, hour, minute, second)
+	return int(time.mktime(dt.timetuple()))
+
+start = create_unix_stamp(2021, 3, 15, 0, 0, 0)
+end = int(time.time())
+
+res = finnhub_client.stock_candles('AAPL', '1', start, end)
 df = pd.DataFrame(res)
 df['t'] = list(map(lambda x: datetime.datetime.fromtimestamp(int(str(x))).strftime('%Y-%m-%d %H:%M:%S'), df.t))
 
 print(df)
+
