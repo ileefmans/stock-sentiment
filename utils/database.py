009978926254edd13d692daae8a5de4e272338bf
Ian Leefmans <iel230@nyu.edu>
2021-03-15 17:54:19 -0400

    Import yml id file

--- utils/database.py
+++ utils/database.py
@@ -1,14 +1,15 @@
 import pymongo
 from pymongo import MongoClient
 import argparse
+import yaml
 from scrape import ScrapeWSB
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Model Options")
     parser.add_argument("stock", type=str, help="stock searched and added to database")
-    parser.add_argument("client_id", type=str, help="Client ID for Reddit API")
-    parser.add_argument("client_secret", type=str, help="Secret Passcode for Reddit API")
+    # parser.add_argument("client_id", type=str, help="Client ID for Reddit API")
+    # parser.add_argument("client_secret", type=str, help="Secret Passcode for Reddit API")
     return parser.parse_args()
 
 
@@ -67,14 +68,20 @@ if __name__ == "__main__":
 	# Get name from argument parser
 	ops = get_args()
 	name = ops.stock
-	client_id = ops.client_id
-	client_secret = ops.client_secret
+	# client_id = ops.client_id
+	# client_secret = ops.client_secret
 
 	# Instantiate Database object
 	database = Database("RedditComments")
 	# Create a collection for desired stock
 	database.create_collection(name)
 	# Instantiate object to scrape Reddit for desired stock
+
+	with open(IDs.yml) as file:
+			IDs = yaml.load(file, Loader=yaml.FullLoader)
+	client_id = IDs['Reddit']['client_id']
+	client_secret = IDs['Reddit']['client_secret']
+
 	getdata = ScrapeWSB(name, 10, 10, client_id, client_secret)
 	# Insert data into database
 	database.insert_document(name, "posts", getdata.process())
