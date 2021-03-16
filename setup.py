6a40b8dfb228e01891f818b192ba127d5d0c1ecb
Ian Leefmans <iel230@nyu.edu>
2021-03-16 19:56:23 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="utils",
+    author="Ian Leefmans",
+    author_email="ian.e.leefmans@gmail.com",
+    url="https://https://github.com/ileefmans/stock-sentiment",
+    packages=["utils"],
+)
