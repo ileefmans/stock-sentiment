2445763492b6ad721f5c037a85a4b42873f993be
Ian Leefmans <iel230@nyu.edu>
2021-03-16 20:33:37 -0400

    Add test for scraping

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,1 +1,9 @@
-#Insert Code Here
+import unittest
+import utils
+
+class test_ScrapeWSB(utittest.TestCase):
+
+	def test_equal_comment_posts(self):
+		self.assertEqual(len(ScrapeWSB("GME", 10, 10).process()), 10)
+	print("DONE SCRAPE TEST")
+t
