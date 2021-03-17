ce837b3e0af4615609a402cab68935647247ea6a
Ian Leefmans <iel230@nyu.edu>
2021-03-16 20:59:44 -0400

    add utils. to function call

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,5 +4,5 @@ import utils
 class test_ScrapeWSB(unittest.TestCase):
 
 	def test_equal_comment_posts(self):
-		self.assertEqual(len(ScrapeWSB("GME", 10, 10).process()), 10)
+		self.assertEqual(len(utils.ScrapeWSB("GME", 10, 10).process()), 10)
 	print("DONE SCRAPE TEST")
