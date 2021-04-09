b2252fe58f75350224e9163082b20f85f8facf55
Ian Leefmans <iel230@nyu.edu>
2021-04-08 20:52:52 -0400

    define argparser and create connection

--- utils/database_rd.py
+++ utils/database_rd.py
@@ -1,3 +1,22 @@
 import mysql.connector
+import argparse
 
-print("done")
+
+def get_args():
+	parser = argparse.ArgumentParser(description="Model Options")
+    parser.add_argument("user", type=str, help="mysql user")
+    parser.add_argument("password", type=str, help="mysql password")
+    parser.add_argument("--host", type=str, default='127.0.0.1', help="mysql host")
+    return parser.parse_args()
+
+
+args  = get_args()
+user = args.user
+password = args.password
+host = args.host
+
+# Establish connection and create cursor
+conn = mysql.connector.connect(user=user, password=password, host=host)
+cursor = conn.cursor()
+cursor.execute("CREATE database STOCKS")
+
