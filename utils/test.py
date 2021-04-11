33c79af6f4eda37d826e9a65df29375fb6dd5f95
Ian Leefmans <iel230@nyu.edu>
2021-04-11 16:44:51 -0400

    Edit test script

--- utils/test.py
+++ utils/test.py
@@ -1,14 +1,34 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient(host="localhost", port=27017) 
+from data import ScrapeWSB
 
-db = client["RedditComments"]
 
-#db["GME"].drop()
 
-print(db.list_collection_names())
-print(db['GME'].find_one({'_id':0}))
+# stock = Stock()
+# stock.set_start([2020, 4, 8, 0, 0, 0])
+# stock.set_end([2020, 4, 8, 2, 0, 0])
+
+
+# print(stock.pull_data("AMC"))
+
+scrapewsb = ScrapeWSB('GME', 10, 10)
+print(scrapewsb.process()[0].keys())
+
+
+
+
+
+#####################################################################################
+
+# client = MongoClient(host="localhost", port=27017) 
+
+# db = client["RedditComments"]
+
+# #db["GME"].drop()
+
+# print(db.list_collection_names())
+# print(db['GME'].find_one({'_id':0}))
 
 # count = 0
 # for i in cursor:
