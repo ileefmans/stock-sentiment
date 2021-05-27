a25a3cdc65bbb9d9d5c1b3d5600531dc3c07c5bf
Ian Leefmans <iel230@nyu.edu>
2021-05-27 16:41:39 -0400

    Create class for pretrained BERT sequence classifier

--- utils/model.py
+++ utils/model.py
@@ -1,1 +1,16 @@
-import huggingface
+import torch
+from torch import nn
+import transformers
+from transformers import BertForSequenceClassification
+
+
+
+class SentimentModel(nn.Module):
+	def __init__(self):
+		super(SentimentModel, self).__init__()
+
+		self.bert = BertForSequenceClassification.from_pretrained('bert-base-cased')
+
+	def forward(self, x):
+
+		return self.bert(x)
