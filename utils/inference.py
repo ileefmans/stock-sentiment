c69a77010cd92b2dff0cb1138c57773cf9a8c003
Ian Leefmans <iel230@nyu.edu>
2021-05-28 13:29:31 -0400

    Start inference script for stock sentiment

---
+++ utils/inference.py
@@ -0,0 +1,20 @@
+import torch
+from datahelper import Dataset, get_indices
+
+
+
+def pull():
+
+	"""
+		Helper function that checks whether or not knew reddit comments and posts need to be scraped
+	"""
+	pass
+
+
+class RunInference:
+	"""
+		Class for running inference to gather stock sentiment
+	"""
+	def __init__(self):
+
+		pass
