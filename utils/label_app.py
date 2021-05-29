155476277f6cf1cbd75f42af2fc734701720848e
Ian Leefmans <iel230@nyu.edu>
2021-05-29 19:11:44 -0400

    Add stock symbol, slider for posts and comments and time filter dropdown to sidebar

--- utils/label_app.py
+++ utils/label_app.py
@@ -1,2 +1,17 @@
 from data import Database, ScrapeWSB
 import streamlit as st
+
+
+st.title('**Labeling Interface**')
+st.markdown(""" #### Interface to assist in hand labeling posts and comments """)
+st.text("")
+st.text("")
+
+stock_id = st.sidebar.text_input("Stock Symbol")
+
+num_posts = st.sidebar.slider("Number of Posts", min_value=1, max_value=100)
+
+num_comments = st.sidebar.slider("Number of Comments", min_value=1, max_value=100)
+
+
+time_filter = st.sidebar.selectbox("Time Filter", ['day', 'week', 'month'], index=0)
