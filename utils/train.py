703ebab0101844fdbd07a14865e3332a73cdda15
Ian Leefmans <iel230@nyu.edu>
2021-05-31 17:20:19 -0400

    Create train and test loaders for commments and posts

--- utils/train.py
+++ utils/train.py
@@ -23,6 +23,50 @@ class Train:
 		self.config = get_config()
 
 		self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-		
+
 		if self.config['model']=='pretrained':
 			self.model = SentimentModel().to(self.device)
+		elif self.config['model'] == 'finetuned':
+			pass ########### INSERT MODEL FOR FINETUNING HERE
+
+		self.indices = get_indices("no_stock_id")
+
+		# Create PyTorch Datasets and instantiate PyTorch Dataloaders
+		self.post_train = PostDataset(self.config['embedding_max_len'], 
+									  self.indices['post_train']
+									 )
+		self.post_test = PostDataset(self.config['embedding_max_len'], 
+									 self.indices['post_test']
+									)
+		self.comment_train = CommentDataset(self.config['embedding_max_len'], 
+											self.indices['comment_train']
+											)
+		self.comment_test = CommentDataset(self.config['embedding_max_len'], 
+										   self.indices['comment_test']
+										   )
+
+		self.post_trainloader = DataLoader(dataset=self.post_train, 
+									 batch_size=self.config['batch_size'], 
+									 num_workers=self.config['num_workers'],
+									 shuffle=self.config['shuffle']
+									 )
+		self.post_testloader = DataLoader(dataset=self.post_test, 
+									 batch_size=self.config['batch_size'], 
+									 num_workers=self.config['num_workers'],
+									 shuffle=self.config['shuffle']
+									 )
+		self.comment_trainloader = DataLoader(dataset=self.comment_train, 
+									 batch_size=self.config['batch_size'], 
+									 num_workers=self.config['num_workers'],
+									 shuffle=self.config['shuffle']
+									 )
+		self.comment_testloader = DataLoader(dataset=self.comment_test, 
+									 batch_size=self.config['batch_size'], 
+									 num_workers=self.config['num_workers'],
+									 shuffle=self.config['shuffle']
+									 )
+
+
+
+
+
