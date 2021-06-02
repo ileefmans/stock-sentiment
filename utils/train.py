Fix calculation of loss and accuracy

utils/train.py
@@ -128,7 +128,7 @@ class Train:
 					# Send input ids and attention masks to device
 					post_input_ids = post_batch['post_input_ids'].to(self.device)
 					post_attention_masks = post_batch['post_attention_mask'].to(self.device)
-					post_targets = post_batch['target'].to(self.device)
+					post_targets = post_batch['target'].reshape(-1,2).to(self.device)
 
 					post_output = self.model(input_ids=post_input_ids, attention_masks=post_attention_masks)
 
@@ -137,10 +137,10 @@ class Train:
 
 					# Make Predictions
 					_, preds = torch.max(post_output, dim=1)
+					_, ground_truths = torch.max(post_targets, dim=1)
 
 					# Calculate Accuracy
-					diff = post_preds - post_targets
-					acc = 1 - ((diff).abs().sum()/len(diff))
+					acc = torch.sum(preds==ground_truths)/len(preds)
 
 					epoch_loss+=loss
 					total_epoch_acc+=acc