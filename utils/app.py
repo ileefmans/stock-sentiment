d112a6d1081e2c3a3317b09bfb10c78b0e9c3cd3
Ian Leefmans <iel230@nyu.edu>
2021-06-14 10:46:49 -0400

    Add scraping and inference with 'loading' spinners and captions

--- utils/app.py
+++ utils/app.py
@@ -8,22 +8,40 @@ from inference import pull, RunInference
 
 
 class App:
-	"""
-		Class for primary application
-	"""
-	def __init__(self):
-		st.title('**Stock Sentiment**')
-		st.markdown(""" #### Application for scraping redit for metrics and visualizations regarding sentiment towards a particular stock """)
-		#st.text("IMPORTANT: You must set sentiment to 'select sentiment' when choosing 'POSTS' or 'COMMENTS'")
-		st.text("")
-		st.text("")
-		st.text("")
+    """
+        Class for primary application
+    """
+    def __init__(self):
+        st.title('**Stock Sentiment**')
+        st.markdown(""" #### Application for scraping redit for metrics and visualizations regarding sentiment towards a particular stock """)
+        #st.text("IMPORTANT: You must set sentiment to 'select sentiment' when choosing 'POSTS' or 'COMMENTS'")
+        st.text("")
+        st.text("")
+        st.text("")
 
 
-		self.stock_id = st.sidebar.text_input("Stock Symbol")
-		self.mode = st.sidebar.selectbox("Mode", ['*Choose Mode*','Visualization', 'Prediction'], index=0)
-		self.go = st.sidebar.button("Go")
+        self.stock_id = st.sidebar.text_input("Stock Symbol")
+        self.mode = st.sidebar.selectbox("Mode", ['*choose mode','Visualization', 'Prediction'], index=0)
+        self.go = st.sidebar.button("Go")
+           
 
+
+    def run(self):
+        if self.go:
+            with st.spinner("Checking to see if database needs to be updated..."):
+                pull(
+                    value=48, 
+                    stock_id=self.stock_id, 
+                    num_posts=10, 
+                    num_comments=5, 
+                    increment='HOUR'
+                )
+
+            with st.stpinner("Running inference..."):
+                run_inference = RunInference(stock_id=self.stock_id)
+                inference_output = run_inference.evaluate()
+
+            
 
 
 
@@ -31,7 +49,10 @@ class App:
 
 
 if __name__ == '__main__':
-	app = App()
+    app = App()
+    app.run()
+
+
 
 
 
