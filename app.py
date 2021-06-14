542bbe9f2d43dc10de70bed234da19c2a8896b95
Ian Leefmans <iel230@nyu.edu>
2021-06-13 21:53:10 -0400

    Move app to utills

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from utils import *
-
-print("Done")
-
---
+++ utils/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from utils import data, datahelper
+#from utils import datahelper
+
+#config = get_config()
+
+#print(config)
+
