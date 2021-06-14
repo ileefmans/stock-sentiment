23800a9a37fdd0969e333043a953dd8c17f1cbb3
Ian Leefmans <iel230@nyu.edu>
2021-06-14 10:09:09 -0400

    Add title and sidebar

--- utils/app.py
+++ utils/app.py
@@ -12,7 +12,17 @@ class App:
 		Class for primary application
 	"""
 	def __init__(self):
-		pass
+		st.title('**Stock Sentiment**')
+		st.markdown(""" #### Application for scraping redit for metrics and visualizations regarding sentiment towards a particular stock """)
+		#st.text("IMPORTANT: You must set sentiment to 'select sentiment' when choosing 'POSTS' or 'COMMENTS'")
+		st.text("")
+		st.text("")
+		st.text("")
+
+
+		self.stock_id = st.sidebar.text_input("Stock Symbol")
+		self.mode = st.sidebar.selectbox("Mode", ['*Choose Mode*','Visualization', 'Prediction'], index=0)
+		self.go = st.sidebar.button("Go")
 
 
 
