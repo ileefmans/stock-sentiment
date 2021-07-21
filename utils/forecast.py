40c1485deb7ac474cfcb81220108f22186a2ea10
Ian Leefmans <iel230@nyu.edu>
2021-07-20 21:19:19 -0400

    Add docstrings

--- utils/forecast.py
+++ utils/forecast.py
@@ -7,6 +7,10 @@ from datetime import datetime
 
 
 def get_start():
+	"""
+		Helper function to retrieve the start of the time period from which to gather stock data
+
+	"""
 	weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 	day = weekdays[datetime.today().weekday()]
 	now = datetime.today()
@@ -23,4 +27,27 @@ def get_start():
 
 	return start
 
-	
+
+class Forecast:
+	"""
+		Class to forecast direction of stock
+	"""
+	def __init__(self, stock_id, type='close'):
+
+		"""
+			Args:
+
+				stock_id (str): Stock symbol of desired stock
+				type (str):		Type of stock price to be predicted, options: ['open', 'high', 'low', 'close']
+
+		"""
+		self.stockid = stock_id
+
+		self.start = get_start()
+
+		self.stock = Stock()
+		self.stock.set_start(self.start)
+
+		self.stock_data = self.stock.pull_data(stock_id)
+
+
