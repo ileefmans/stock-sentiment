d75cd890edcfe2adb43d6c4bffcc44690fbdad45
Ian Leefmans <iel230@nyu.edu>
2021-07-20 20:23:44 -0400

    Start helper function for getting time interval

--- utils/forecast.py
+++ utils/forecast.py
@@ -1,2 +1,16 @@
 from data import Stock
+from datetime import datetime
 
+
+
+
+
+
+def get_interval():
+	weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+	day = weekdays[datetime.today().weekday()]
+
+	if day in ['Saturday', 'Sunday']:
+
+
+	else:
