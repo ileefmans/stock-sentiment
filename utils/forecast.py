685b5566dedd3ef79aca43ea0ab6fb0bada28635
Ian Leefmans <iel230@nyu.edu>
2021-07-20 20:48:51 -0400

    Finish helper function for getting stock data start

--- utils/forecast.py
+++ utils/forecast.py
@@ -6,11 +6,21 @@ from datetime import datetime
 
 
 
-def get_interval():
+def get_start():
 	weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 	day = weekdays[datetime.today().weekday()]
+	now = datetime.today()
+	if day in ['Thursday', 'Friday', 'Saturday']:
+		startdt = now-timedelta(days=3)
 
-	if day in ['Saturday', 'Sunday']:
+	elif day in ['Monday', 'Tuesday', 'Wednesday']:
+		startdt = now-timedelta(days=5)
 
+	elif day == 'Sunday':
+		startdt = now-timedelta(days=4)
 
-	else:
+	start = [startdt.year, startdt.month, startdt.day, startdt.hour, startdt.minute, startdt.second]
+
+	return start
+
+	
